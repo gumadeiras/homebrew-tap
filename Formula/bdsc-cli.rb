fad4917467407556954a5e2e7baebdaac5d8957a
Gustavo Madeira Santana <gumadeiras@gmail.com>
2026-05-13 14:08:02 -0400

    Update bdsc-cli to 0.2.2
    Point the formula at the v0.2.2 release sdist and update the SHA256 from the published GitHub release asset.

--- Formula/bdsc-cli.rb
+++ Formula/bdsc-cli.rb
@@ -3,8 +3,8 @@ class BdscCli < Formula
 
   desc "Sync and query BDSC datasets locally"
   homepage "https://github.com/gumadeiras/bdsc-cli"
-  url "https://github.com/gumadeiras/bdsc-cli/releases/download/v0.2.1/bdsc_cli-0.2.1.tar.gz"
-  sha256 "458b0dc3fa0388fe0fe81647c6573ecd2d3ea1cc23e32166a8fbed228d932ded"
+  url "https://github.com/gumadeiras/bdsc-cli/releases/download/v0.2.2/bdsc_cli-0.2.2.tar.gz"
+  sha256 "710222e21863b8a7b300a3a73ce28bd96ada3bd28ab610e066f9dc0ddce1cb79"
   license "MIT"
 
   depends_on "python@3.13"
